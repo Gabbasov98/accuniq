dc77c8144a611657d7c90d348fb4fa80ef7a521b
Marsel Gabbasov <marsel13_98@mail.ru>
2022-05-30 17:11:24 +0500

    sdds

--- src/js/01_main.js
+++ src/js/01_main.js
@@ -22,7 +22,7 @@ function gallery() {
 }
 
 $(document).ready(function() {
-    $('input[type="tel"]').mask('+7 999 999-9999', { placeholder: '+7             ' });
+    $('input[type="tel"]').mask('+7 999 9999999', { autoclear: false }, { placeholder: '+7            ' });
     gallery()
 
 
