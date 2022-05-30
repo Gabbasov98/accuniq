00e8f3a65384df2ec0bcc592cbbeb89166896d73
Marsel Gabbasov <marsel13_98@mail.ru>
2022-05-30 12:30:56 +0500

    dfdf

--- src/js/01_main.js
+++ src/js/01_main.js
@@ -29,6 +29,7 @@ $(document).ready(function() {
     $(".header__burger").click(function() {
         $(this).toggleClass("header__burger--active")
         $(".header__mob").toggleClass("header__mob--active")
+        $("body").toggleClass("fixed-body")
     })
 
 
@@ -67,4 +68,14 @@ $(document).ready(function() {
         }
     }
 
+
+    $('.video').parent().click(function() {
+        if ($(this).children(".video").get(0).paused) {
+            $(this).children(".video").get(0).play();
+            $(this).children(".playpause").fadeOut();
+        } else {
+            $(this).children(".video").get(0).pause();
+            $(this).children(".playpause").fadeIn();
+        }
+    });
 });
